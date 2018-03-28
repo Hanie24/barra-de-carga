ad1495ec90e2c464b7ac5f80553f4091921f695a
Hanie24 <sailor_121108@hotmail.com>
2018-03-28 10:09:19 -0600

    comienzo

---
+++ READMY.md
@@ -0,0 +1,4 @@
+**CONCEPTOS**
+
+***super()***
+*es usada para llamar funciones de un objeto padre. on válidas en cualquier definición de método tanto para clases como para objetos literales.*
---
+++ app.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+class countdown extends Component {
+ constructor() {
+    super()
+ }
+ progressBar()
+    this.
+}
